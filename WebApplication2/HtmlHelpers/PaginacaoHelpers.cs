3857c765a4f011a23e05e90b4dfb61b9081ef9ee
Ketlyn Izidorio <ketlynizidorio18@gmail.com>
2017-10-27 23:26:32 -0200

    Inserção de paginação

---
+++ WebApplication2/HtmlHelpers/PaginacaoHelpers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.HtmlHelpers
+{
+    public static class PaginacaoHelpers
+    {
+       //Total de Paginas = 3 
+       // Monta 3vz o H
+        public static MvcHtmlString PageLinks (this HtmlHelper html, Paginacao paginacao, Func<int, string> paginaUrl)
+        {
+            StringBuilder resultado = new StringBuilder();
+            for (int i = 1; i <= paginacao.TotalPagina; i++)
+            {
+                //Classe TagBuilder monta a tag
+                TagBuilder tag = new TagBuilder("a");
+                //Adiciono atributo href
+                tag.MergeAttribute("href", paginaUrl(i));
+                tag.InnerHtml = i.ToString();
+
+                //Adicionando cor ao botão que esta na pagina atual - Btn-primary é o css que faz o botão ficar com a cor diferente   
+
+                if (i == paginacao.PaginaAtual)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+                tag.AddCssClass("btn btn-default");
+                resultado.Append(tag);
+            }
+            return MvcHtmlString.Create(resultado.ToString());
+        }
+    }
+}
---
+++ WebApplication2/Models/Paginacao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Paginacao
+    {
+        //Quantos itens tenho no banco
+        public int ItensTotal { get; set; }
+
+        //Quantos itens de lanches por página
+        public int ItensPorPagina { get; set; }
+
+        //Pagina exibida no momento
+        public int PaginaAtual { get; set; }
+
+        //Quantas paginas vou ter 
+        public int TotalPagina
+        {
+               get { return (int)Math.Ceiling((decimal)ItensTotal / ItensPorPagina); }
+        }
+    }
+}
